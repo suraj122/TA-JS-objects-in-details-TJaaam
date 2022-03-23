4214cb09e99b960976aa01b21b6f8e7f336b0535
suraj_122 <122suraj0@gmail.com>
2022-03-23 16:48:12 +0530

    finish asssignment

---
+++ block-BJaaic/code/index.js
@@ -0,0 +1,95 @@
+// Animals
+
+let animalMethods = {
+  eat: function () {
+    console.log(`I live in ${this.location} and I can eat`);
+  },
+  changeLocation: function (newLocation) {
+    this.location = newLocation;
+    return this.location;
+  },
+  summary: function () {
+    return `I live in ${this.location} and I have ${this.numberOfLegs} legs`;
+  },
+};
+
+// Dog Methods:
+
+// - `bark()` - alerts a message saying `I am ${name} and I can bark 🐶`
+// - `changeName(changeName)` - accepts the name property and updates the name of the dog
+// - `changeColor(newColor)` - accepts the new color and updates the color of the dog
+// - `summary()` - returns `I am ${name} and I am of ${color} color. I can also bark`
+
+let dogMethods = {
+  bark: function () {
+    alert(`I am ${this.name} and I can bark 🐶`);
+  },
+  changeName: function (name) {
+    this.name = name;
+    return this.name;
+  },
+  changeColor: function (color) {
+    this.color = color;
+    return this.color;
+  },
+  dogSummary: function () {
+    return `I am ${this.name} and I am of ${this.color} color. I can also bark`;
+  },
+};
+Object.setPrototypeOf(dogMethods, animalMethods);
+
+// Cat Methods:
+
+let catMethods = {
+  meow: function () {
+    alert(`I am ${this.name} and I can do mewo meow 😹`);
+  },
+  changeName: function (name) {
+    this.name = name;
+    return this.name;
+  },
+  changeColorOfEyes: function (color) {
+    this.color = color;
+    return this.color;
+  },
+  catSummary: function () {
+    return `I am ${this.name} and the color of my eyes are ${this.colorOfEyes} color. I can also meow meow`;
+  },
+};
+
+Object.setPrototypeOf(catMethods, animalMethods);
+
+// Create Animal
+function createAnimal(location, numberOfLegs) {
+  let animal = Object.create(catMethods);
+  animal.location = location;
+  animal.numberOfLegs = numberOfLegs;
+  return animal;
+}
+
+// Create Dog
+
+function createDog(location, numberOfLegs, name, color) {
+  let animal = Object.create(dogMethods);
+  animal.location = location;
+  animal.numberOfLegs = numberOfLegs;
+  animal.name = name;
+  animal.color = color;
+  return animal;
+}
+
+// Create Cat
+
+function createCats(location, numberOfLegs, name, colorOfEyes) {
+  let cat = Object.create(catMethods);
+  cat.location = location;
+  cat.numberOfLegs = numberOfLegs;
+  cat.name = name;
+  cat.colorOfEyes = colorOfEyes;
+  return cat;
+}
+
+let animal1 = createAnimal("Australia", 2);
+let animal2 = createDog("AltCampus", 4, "Pochi", "Black");
+
+let animal3 = createCats("AltCampus", 3, "PussyCat", "Blue");
